6e38c959666910a601a347b70744c8649dc054ec
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-22 18:42:14 +0300

    Look back and refactor

--- add_refactor.js
+++ add_refactor.js
@@ -1,14 +1,10 @@
 function charCount(str) {
   var obj = {};
-  for (var i = 0; i < str.length; i++) {
-    var char = str[i].toLowerCase();
+  for (var char of str) {
+    char = char.toLowerCase();
     // if char is something else (space, period, etc.) don't do anything
     if (/[a-z0-9]/.test(char)) {
-      if (obj[char] > 0) {
-        obj[char]++;
-      } else {
-        obj[char] = 1;
-      };
+      obj[char] = ++obj[char] || 1;
     }
   }
   return obj;
