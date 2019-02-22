b1c61bbb125f7642080706fd37286301ea632c6d
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-22 18:55:17 +0300

    Look back and refactor

--- add_refactor.js
+++ add_refactor.js
@@ -1,13 +1,24 @@
 function charCount(str) {
   var obj = {};
   for (var char of str) {
-    char = char.toLowerCase();
-    // if char is something else (space, period, etc.) don't do anything
-    if (/[a-z0-9]/.test(char)) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
       obj[char] = ++obj[char] || 1;
     }
   }
   return obj;
+}
+
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123) // lower alpha (a-z)
+  ) {
+    return false;
+  }
+  return true;
 }
 
 console.log(charCount("hello"));
