9d7b53c13169e7db0988e2e19da350cac2455515
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-22 15:51:57 +0300

    Explore Concrete Ecamples

--- add.js
+++ add.js
@@ -1,23 +1,11 @@
-// ===============================================================
-// Write a function which takes two numbers and returns their sum.
-// ===============================================================
+// ============================================================================================
+// Write a function which takes in a string and returns counts of each character in the string.
+// ============================================================================================
 
-// 1. Can I restate the problem in my own words?
-// "implement addition"
+charCount("aaaa"); // {a:4}
+charCount("hello"); // {h:1, e:1, l:2, o:1}
 
-// 2. What are the inputs that go into the problem?
-// - ints?
-// - floats?
-// - what about string for large numbers?
+// "my phone number is 23412123"
+// "Hello hi"
 
-// 3. What are the outputs that should come from the solution to the problem?
-// - ints?
-// - floats?
-// - what about string for large numbers?
-
-// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you set about solving the problem. That's okay; it's still worth considering the question at this early stage.)
-// ""
-
-// 5. How should I label the important pieces of data that are a part of the problem?
-// ""
-
+charCount(""); // ? {} ? null ? false ? undefined ? error ?
