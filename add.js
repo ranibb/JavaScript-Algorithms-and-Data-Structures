b6b0e9c32571efb687f1c4ac3777c46658d88597
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-22 18:22:28 +0300

    Solve or simplify

--- add.js
+++ add.js
@@ -2,12 +2,12 @@
 // Write a function which takes in a string and returns counts of each character in the string.
 // ============================================================================================
 
-charCount("aaaa");
+// charCount("aaaa");
 /*{
   "a": 4
 }*/
 
-charCount("hello");
+// charCount("hello");
 /*{
   h: 1,
   e: 1,
@@ -15,7 +15,7 @@ charCount("hello");
   o: 1
 }*/
 
-charCount("Your PIN number is 1234!");
+// charCount("Your PIN number is 1234!");
 /*{
   1: 1,
   2: 1,
@@ -34,16 +34,31 @@ charCount("Your PIN number is 1234!");
   y: 1,
 }*/
 
-function charCount(str) {
+// function charCount(str) {
   // do something
   // return an object with keys that are lowercase alphanumeric characters in the string; values should be the counts for those characters
-}
+// }
 
 function charCount(str) {
   // make object to return at end
+  var result = {};
   // loop over string, for each character...
+  for(var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
     // if the char is a number/letter AND is a key in object, add one to count
+    if(result[char] > 0) {
+      result[char]++;
+    } 
     // if the char is a number/letter AND not in object, add it object and set values to 1
+    else {
+      result[char] = 1;
+    }
+  }
     // if char is something else (space, period, etc.) don't do anything
   // return onject at end
+  return result;
 }
+
+console.log(charCount("hello"));
+
+console.log(charCount("Hi there!"));
