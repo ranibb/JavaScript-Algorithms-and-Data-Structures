525bba1a80181c72f925b4ce21ec83079551abab
Rani Abu Sultan <raniabusultan@gmail.com>
2019-03-11 19:22:50 +0300

    Our Third Recursive Function

---
+++ factorial.js
@@ -0,0 +1,19 @@
+// Writing Factorial Iteratively
+function factorialIteratively(num) {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total *= i
+  }
+  return total;
+}
+
+console.log(factorialIteratively(4));
+
+
+// Writing Factorial Recursively
+function factorialRecursively(num) {
+  if (num === 1) return 1;
+  return num * factorialRecursively(num-1);
+}
+
+console.log(factorialRecursively(5));
