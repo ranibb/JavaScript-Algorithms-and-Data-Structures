212cf511b9c7cd84db3e9598832c6d282771c80b
Rani Abu Sultan <raniabusultan@gmail.com>
2019-05-10 17:11:30 +0300

    PUSHING: Adding a node to the end of the DLL

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,3 +1,13 @@
+/** Pushing pseudocode
+ * 🔸 Create a new node with the value passed to the function
+ * 🔸 If the head property is null set the head and tail to be the newly created node 
+ * 🔸 If not, set the next property on the tail to be that node
+ * 🔸 Set the previous property on the newly created node to be the tail
+ * 🔸 Set the tail to be the newly created node
+ * 🔸 Increment the length
+ * 🔸 Return the Doubly Linked List
+ */
+
 class Node {
   constructor(val) {
     this.val = val;
@@ -12,10 +22,30 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-first = new Node(12);
-first.next = new Node(13);
-first.next = new Node(13);
+// first = new Node(12);
+// first.next = new Node(13);
+// first.next = new Node(13);
+// console.log(first);
 
-console.log(first);
+list = new DoublyLinkedList();
+
+list.push(99);
+list.push(100);
+list.push("Last Item");
+console.log(list);
+
