f955a77d556254af4ed505e4f6c9c9cb66ec6361
Rani Abu Sultan <raniabusultan@gmail.com>
2019-05-25 13:06:47 +0300

    Hash Table Get Solution

--- hash-table.js
+++ hash-table.js
@@ -4,8 +4,15 @@
  * 3. Stores the key-value pair in the hash table array via separate chaining
  */
 
+/** Hash Table Set
+ * 1. Accepts a key
+ * 2. Hashes the key
+ * 3. Retrieves the key-value pair in the hash table
+ * 4. If the key isn't found, returns undefined
+ */
+
 class HashTable {
-  constructor(size = 4) {
+  constructor(size = 53) { // Default size is 53
     this.keyMap = new Array(size);
   }
 
@@ -28,13 +35,35 @@ class HashTable {
     this.keyMap[index].push([key, value]);
     return index;
   }
+
+  get(key) {
+    let index = this._hash(key);
+    if(this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if(this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
-let ht = new HashTable();
+// let ht = new HashTable(4);
+// console.log(ht.set("hello world", "goodbye!!")); // 0
+// console.log(ht.set("dogs", "are cool")); // 3
+// console.log(ht.set("cats", "are fine")); // 1
+// console.log(ht.set("i love", "pizza")); // 1
+// console.log(ht); // [ [ [Array] ], [ [Array], [Array] ], <1 empty item>, [ [Array] ] ] }
 
-// if you set size = 4 in the constructor
-console.log(ht.set("hello world", "goodbye!!")); // 0
-console.log(ht.set("dogs", "are cool")); // 3
-console.log(ht.set("cats", "are fine")); // 1
-console.log(ht.set("i love", "pizza")); // 1
-console.log(ht); // [ [ [Array] ], [ [Array], [Array] ], <1 empty item>, [ [Array] ] ] }
+let ht = new HashTable(17);
+ht.set("maroon","#800000");
+ht.set("yellow","#FFFF00");
+ht.set("olive","#808000");
+ht.set("salmon","#FA8072");
+ht.set("lightcoral","#F08080");
+ht.set("mediumvioletred","#C71585");
+ht.set("plum","#DDA0DD");
+
+console.log(ht.get("yellow")); // #FFFF00
+console.log(ht.get("plum")); // #DDA0DD
