a5b533b7a0fafcd6796a5a701e22ef6682ed85aa
Rani Abu Sultan <raniabusultan@gmail.com>
2019-06-02 04:07:39 +0300

    Remove Vertex & Remove Edge

--- graph.js
+++ graph.js
@@ -11,6 +11,20 @@
  * 🔸 Don't worry about handling errors/invalid vertices
  */
 
+/** REMOVING AN EDGE
+ * 🔸 This function should accept two vertices, we'll call them vertex1 and vertex2
+ * 🔸 The function should reassign the key of vertex1 to be an array that does not contain vertex2
+ * 🔸 The function should reassign the key of vertex2 to be an array that does not contain vertex1
+ * 🔸 Don't worry about handling errors/invalid vertices
+ */
+
+/** REMOVING A VERTEX
+ * 🔸 The function should accept a vertex to remove
+ * 🔸 The function should loop as long as there are any other vertices in the adjacency list for that vertex
+ * 🔸 Inside of the loop, call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+ * 🔸 delete the key in the adjacency list for that vertex
+ */
+
 class Graph {
   constructor() {
     this.adjacencyList = {};
@@ -22,15 +36,42 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+  }
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length) {
+      const adjacentVertix = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertix);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 var g = new Graph();
 g.addVertex("Dallas");
 g.addVertex("Tokyo");
 g.addVertex("Aspen");
-console.log(g);
+// console.log(g);
 
 g.addEdge("Dallas", "Tokyo");
-console.log(g);
+// console.log(g);
 g.addEdge("Dallas", "Aspen");
-console.log(g);
+// console.log(g);
+
+// g.removeEdge("Dallas", "Aspen");
+// console.log(g);
+// g.removeEdge("Dallas", "Tokyo");
+// console.log(g);
+
+g.addVertex("Los Angeles");
+g.addVertex("Hong Kong");
+g.addEdge("Hong Kong", "Tokyo");
+g.addEdge("Hong Kong", "Dallas");
+g.addEdge("Los Angeles", "Hong Kong");
+g.addEdge("Los Angeles", "Aspen");
+
+console.log(g.adjacencyList);
+g.removeVertex("Hong Kong");
+console.log(g.adjacencyList);
